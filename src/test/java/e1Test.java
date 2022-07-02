9ccd93ad35b88df74f8baf5ba45d389cf9269e63
John Doe <92479816+AndreVA21@users.noreply.github.com>
2022-07-02 00:09:44 -0400

    segundo test

--- src/test/java/e1Test.java
+++ src/test/java/e1Test.java
@@ -6,8 +6,11 @@ class e1Test {
     e1 prueba = new e1();
 
     @Test
-    public void testelevar1()
+    public void testelevar1_1()
     {
         assertEquals(8,prueba.elevar1(2,3));
     }
+
+    @Test
+    public void testelevar1_2(){assertEquals(125,prueba.elevar1(5,3));}
 }
